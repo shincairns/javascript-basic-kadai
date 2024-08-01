52ea70e37646dcfe4b66b546bcc8c906d84ff87e
shincairns <shin@cymbal.tv>
2024-08-01 11:49:31 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,7 @@
 console.log(document.getElementById('text'));
 
 const displayText = document.getElementById("text")
+const button = document.getElementById("btn");
 
 btn.addEventListener('click', () => {
   displayText.textContent='ボタンをクリックしました';
